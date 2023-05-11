c199819f5e6d55f225781f9c4ee2d25422753bc5
Aniket yadav <93505070+AniketYadav11@users.noreply.github.com>
2023-05-11 12:34:47 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore } from 'redux';
+import contactReducer from './redux/reducers/contactReducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+
+const store = createStore(contactReducer, composeWithDevTools());
+
+
+const root = createRoot(document.querySelector('#root'));
+
+root.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+);
+
+
+
+// import React from 'react';
+
+// import './index.css';
+// import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
